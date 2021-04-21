c8d37c8b52c446b103a8a3d37bf7033ce992a31c
RajeevKumarSinha <rajeev.exousia@gmail.com>
2021-04-21 23:07:42 +0530

    errors update

--- js/filter.js
+++ js/filter.js
@@ -1,76 +1,76 @@
 let bakedItems = [{
     image: "images/ardino.jpg",
     name: 'Ardino',
-    price: '$5',
+    price: 'Rs.50',
     category: "pcb"
 },
 {
     image: 'images/breadboard.jpg',
     name: 'Breadboard',
-    price: '$5',
+    price: 'Rs.50',
     category: "pcb"
 }, {
     image: 'images/raspberry.jpg',
     name: 'Raspberry pi v4',
-    price: '$5',
+    price: 'Rs.50',
     category: "pcb"
 },
 {
-    image: 'images/ic.jpg',
+    image: 'images/IC.jpg',
     name: 'IC',
-    price: '$5',
+    price: 'Rs.200',
     category: "parts"
 },
 {
     image: 'images/resistors.png',
     name: 'Resistors',
-    price: '$10',
+    price: 'Rs.200',
     category: "parts"
 },
 {
     image: 'images/wires.jpg',
     name: 'Led(pack of 8)',
-    price: '$10',
+    price: 'Rs.200',
     category: "parts"
 }, {
     image: 'images/led.jpg',
     name: 'Wires(pack of 50)',
-    price: '$10',
+    price: 'Rs.200',
     category: "parts"
 },
 {
     image: 'images/screwdriver.png',
     name: 'Screw Driver',
-    price: '$10',
+    price: 'Rs.150',
     category: "tools"
 },
 {
     image: 'images/solderingiron.png',
     name: 'Soldering Iron',
-    price: '$15',
+    price: 'Rs.150',
     category: "tools"
 },
 {
     image: 'images/wireclipper.jpg',
     name: 'Wire Clipper',
-    price: '$15',
+    price: 'Rs.200',
     category: "tools"
 }, {
     image: 'images/firealarmkit.jpg',
     name: 'Fire Alarm Kit',
-    price: '$15',
+    price: 'Rs.550',
     category: "kit"
 },
 {
     image: 'images/motionsensorkit.jpg',
     name: 'Motion Sensor Kit',
-    price: '$15',
+    price: 'Rs.550',
     category: "kit"
 },
 {
     image: 'images/wirelesschargingkit.jpg',
     name: 'Wireless Charging Kit',
-    price: '$15',
+    price: 'Rs.550',
     category: "kit"
 }];
 
